5831ff212b99e3fbd20c14299ee71ba7dfa48e1b
marguerite-blondet <marguerite.blondet@edu.esiee.fr>
2026-09-22 10:49:32 +0200

    second commit

--- population.py
+++ population.py
@@ -64,6 +64,120 @@ def read_file(filename):
         return list(reader)
 
 
+def build_list_departements(data):
+    """retourne la liste ordonnée des départements contenus dans les données
+
+    Args:
+        data (list): la liste de dictionnaires retournée par read_file()
+
+    Returns:
+        list: la liste ordonnée des tuples (code dpt, nom dpt)
+    
+    >>> data = read_file(FILENAME)
+    >>> ld = build_list_departements(data)
+    >>> type(ld)
+    <class 'list'>
+    >>> len(ld)
+    99
+    >>> '15' in ld
+    False
+    >>> 15 in ld
+    False
+    >>> 'Cantal' in ld
+    False
+    >>> ('15', 'Cantal') in ld
+    True
+    >>> ld[::20]
+    [('01', 'Ain'), ('22', "Côtes-d'Armor"), ('40', 'Landes'), ('60', 'Oise'), ('80', 'Somme')]
+    >>> ld[5::20]
+    [('06', 'Alpes-Maritimes'), ('27', 'Eure'), ('45', 'Loiret'), ('65', 'Hautes-Pyrénées'), ('85', 'Vendée')]
+    >>> ld[10::20]
+    [('11', 'Aude'), ('30', 'Gard'), ('50', 'Manche'), ('70', 'Haute-Saône'), ('90', 'Territoire de Belfort')]
+    >>> ld[15::20]
+    [('16', 'Charente'), ('35', 'Ille-et-Vilaine'), ('55', 'Meuse'), ('75', 'Paris'), ('95', "Val-d'Oise")]
+    >>> ld[20::20]
+    [('22', "Côtes-d'Armor"), ('40', 'Landes'), ('60', 'Oise'), ('80', 'Somme')]
+    >>> ld[5]
+    ('06', 'Alpes-Maritimes')
+    >>> ld[15]
+    ('16', 'Charente')
+    >>> ld[25]
+    ('27', 'Eure')
+    >>> ld[35]
+    ('35', 'Ille-et-Vilaine')
+    >>> ld[45]
+    ('45', 'Loiret')
+    >>> ld[55]
+    ('55', 'Meuse')
+    >>> ld[65]
+    ('65', 'Hautes-Pyrénées')
+    """
+    # votre code ici
+    ld = set()
+    for row in data:
+        cp = row.get("Code Officiel Département")
+        nom = row.get("Nom Officiel Département")
+        if cp and nom :
+            ld.add((cp, nom))
+    return sorted(list(ld))
+
+
+def build_list_communes(data):
+    """retourne la liste des tuples (code, commune)
+
+    Args:
+        data (list): la liste de dictionnaires retournée par read_file()
+
+    Returns:
+        list: la liste ordonnée des tuples (code commune, nom commune)
+    
+    >>> data = read_file(FILENAME)
+    >>> lc = build_list_communes(data)
+    >>> type(lc)
+    <class 'list'>
+    >>> len(lc)
+    35836
+    >>> type(lc[999])
+    <class 'tuple'>
+    >>> len(lc[999])
+    2
+    >>> lc[999]
+    ('02598', 'Pernant')
+    >>> type(lc[999][0])
+    <class 'str'>
+    >>> type(lc[999][1])
+    <class 'str'>
+    >>> lc[100:103]
+    [('01105', 'Civrieux'), ('01106', 'Cize'), ('01107', 'Cleyzieu')]
+    >>> lc[900:903]
+    [('02499', 'Montbavin'), ('02500', 'Montbrehain'), ('02501', 'Montchâlons')]
+    >>> lc[2000:2003]
+    [('06089', 'Opio'), ('06090', 'Pégomas'), ('06091', 'Peille')]
+    >>> lc[5000:5003]
+    [('14712', 'Troarn'), ('14713', 'Montillières-sur-Orne'), ('14713', 'Trois-Monts')]
+    >>> lc[10000:10003]
+    [('27522', 'Saint-Christophe-sur-Condé'), ('27523', "Saint-Clair-d'Arcey"), ('27524', 'Sainte-Colombe-la-Commanderie')]
+    >>> lc[15000:15003]
+    [('39138', 'Chemin'), ('39139', 'Chêne-Bernard'), ('39140', 'Chêne-Sec')]
+    >>> lc[20000:20003]
+    [('54062', 'Benney'), ('54063', 'Bernécourt'), ('54064', 'Bertrambois')]
+    >>> lc[25000:25003]
+    [('63001', 'Aigueperse'), ('63002', 'Aix-la-Fayette'), ('63003', 'Ambert')]
+    >>> lc[30000:30003]
+    [('76002', 'Alvimare'), ('76004', 'Ambrumesnil'), ('76005', 'Amfreville-la-Mi-Voie')]
+    >>> lc[-3:]
+    [('97502', 'Saint-Pierre'), ('97701', 'Saint-Barthélemy'), ('97801', 'Saint-Martin')]
+    """
+    # votre code ici
+    lc = set()
+    for row in data :
+        cp = row.get("Code Officiel Commune / Arrondissement Municipal")
+        nom = row.get("Nom Officiel Commune / Arrondissement Municipal")
+        if cp and nom :
+            lc.add((cp, nom))
+    return sorted(list(lc))
+
+
 
 def main():
     # votre code de test ici
@@ -72,17 +186,19 @@ def main():
     #pass
     # Exemples d'appels
     data = read_file(FILENAME)
-    # l = build_list_departements(data)
-    # c = build_list_communes(data)
+    l = build_list_departements(data)
+    c = build_list_communes(data)
     # p = get_pop_commune(data, '39124')
     # d = build_dict_departements(data)
     # s = stat_by_dpt(d, '77')
+    print(f"Nombre de communes : {len(c)}")
+    print("Exemple :", c[999])
 
     
 # Ne pas modifier le code ci-dessous
 if __name__ == '__main__':
-    dt = True
-    #dt = False # Décommenter pour exécuter "main()"
+    #dt = True
+    dt = False # Décommenter pour exécuter "main()"
     if dt:
         # la ligne suivante teste la fonction "read_file"
         doctest.run_docstring_examples(read_file, globals(), verbose=True)
